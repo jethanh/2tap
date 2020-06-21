5e8e4e6e9ad5c7c7c0f5af35086fe8de438f54ce
unknown <jethanhargrave@gmail.com>
2020-06-21 10:00:19 -0400

    scrollbar styles

--- src/styles/Global.js
+++ src/styles/Global.js
@@ -14,6 +14,26 @@ const GlobalStyles = createGlobalStyle`
 
   html {
     font-size: 62.5%;
+    /* width */
+::-webkit-scrollbar {
+  width: 5px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 5px grey; 
+  border-radius: 10px;
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: red; 
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #2d3436; 
+}
   }
 
   body {
@@ -26,6 +46,8 @@ const GlobalStyles = createGlobalStyle`
     display: flex;
     flex-direction: column;
   }
+
+  
   
 
 `;
