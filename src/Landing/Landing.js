4744305f623edac319482079b48d99d43e4adc72
unknown <jethanhargrave@gmail.com>
2020-09-30 03:58:55 -0400

    error debugging

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import styled from "styled-components";
-import axios from 'axios';
-import { useState, useEffect } from 'react';
 import '../App.css'
 import './betterline.png'
 
