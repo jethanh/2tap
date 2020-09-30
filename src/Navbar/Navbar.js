b9604232e81334a3eba80c9b12c2b5b00d998e80
unknown <jethanhargrave@gmail.com>
2020-09-30 06:55:10 -0400

    error fix

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -10,12 +10,6 @@ const Navbar = (props) => {
     transform: 'translate3d(0, 0, 0)',
   });
 
-  const linkAnimation = useSpring({
-    from: { transform: 'translate3d(0, 30px, 0)', opacity: 0 },
-    to: { transform: 'translate3d(0, 0, 0)', opacity: 1 },
-    delay: 800,
-    config: config.wobbly,
-  });
 
   return (
     <>
