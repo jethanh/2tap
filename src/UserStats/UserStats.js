8fb0343870faa0ef06e2e70b561a2e1dd91592ed
unknown <jethanhargrave@gmail.com>
2020-10-05 16:08:38 -0400

    unused variables

--- src/UserStats/UserStats.js
+++ src/UserStats/UserStats.js
@@ -1,5 +1,4 @@
-import { findAllByTestId } from '@testing-library/react';
-import React, {useEffect, useState} from 'react'
+import React, {useEffect} from 'react'
 import {useParams} from 'react-router-dom';
 import * as valdata from '../data/valdata.json';
 
@@ -10,33 +9,27 @@ const UserStats = ({user, tag}) => {
     const userData = valdata.players.map(player => {
         if (player.puuid === "fpco9wmv9xvNlZsB16ChQyNkuASzzUJFSITHYzC5mDctuVls_7tGecmkfDk53G3mgVPWvnFUUy95_Q") {
             return player
-    }})
+        }
+    })
 
     const arr = []
     console.log(userData[0].stats)
-
     arr.push(userData[0])
-
     console.log(arr)
 
-
-
-
-        useEffect(() => {
+     useEffect(() => {
         if (userData[0].teamId === "Red") {
             if(valdata.teams[0].teamId === "Red") {
                 return arr[0].won = valdata.teams[0].won
             }
-
         } else if (userData[0].teamId === "Blue") {
             if(valdata.teams[1].teamId === "Blue"){
                 return arr[0].won = valdata.teams[1].won
-
             }
         } else {
             return null
-        }
-    })
+            }
+        })
 
   return (
     <>
